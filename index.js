839b6759040ffd9b474341ed6ebbe0fc157df7a9
SaranjeetSingh19 <ss5259603@gmail.com>
2024-06-13 11:03:37 +0530

    Deployment 0.3

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
+app.get("/demo", (req, res) => {
+  res.status(200).send("Testing...")
+})
+
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
