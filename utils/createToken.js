7b04b1521a315eeb7e1af2126acc58fd73b1f842
SaranjeetSingh19 <ss5259603@gmail.com>
2024-06-14 13:38:55 +0530

    Deployment 0.9

--- utils/createToken.js
+++ utils/createToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV !== "DEVELOPMENT",
+    secure: process.env.NODE_ENV !== "DEVELOPMENT",
     sameSite: "strict",
     maxAge: 2 * 24 * 60 * 60 * 1000,
   });
