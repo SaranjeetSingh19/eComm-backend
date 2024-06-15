e3282e9b07c0c7f80ffac580f5bedf1c95255725
SaranjeetSingh19 <ss5259603@gmail.com>
2024-06-16 00:35:58 +0530

    Deployment 0.14

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-import path from "path";
-import express from "express";
-import dotenv from "dotenv";
-import Stripe from "stripe";
-import cors from "cors";
 import cloudinary from "cloudinary";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
 import { connectDb } from "./config/db.js";
-import userRoutes from "./routes/user.route.js";
 import categoryRoutes from "./routes/category.route.js";
+import orderRoutes from "./routes/order.route.js";
 import productRoutes from "./routes/product.route.js";
 import uploadRoutes from "./routes/upload.route.js";
-import orderRoutes from "./routes/order.route.js";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -30,16 +29,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5000",
-      process.env.CLIENT_URL,
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "http://localhost:5000",
+//       process.env.CLIENT_URL,
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+const corsOptions = {
+  origin: 'https://shifsy.vercel.app', // Replace with your Vercel frontend domain
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
